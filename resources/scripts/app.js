3789975f17a483ba7c7112cf5e4d59b3f2b9d685
William Hoffman <whoffman1989@gmail.com>
2020-02-04 23:33:22 -0500

    added design section - images hosted

---
+++ resources/scripts/app.js
@@ -0,0 +1,32 @@
+// Typer Animation
+
+const typer = new TypeIt("#typed", {
+  speed: 60,
+  breakLines: false,
+  waitUntilVisible: true,
+  loop: true
+})
+  .type("web developer.")
+  .pause(3000)
+  .delete()
+  .type("creator.")
+  .pause(3000)
+  .delete()
+  .type("always learning.")
+  .pause(3000)
+  .delete()
+  .type("never quitting.")
+  .pause(3000)
+  .go();
+
+// about page typing animation
+
+const typerAbout = new TypeIt("#typedAbout", {
+  speed: 60,
+  breakLines: false,
+  waitUntilVisible: true
+})
+  .type(
+    "passionate web developer living in indianapolis, in. lets connect ↓↓↓↓"
+  )
+  .go();
